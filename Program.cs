fbff273159b3b2c74c20a86410b0463445cebde4
Tejora <numan.992@gmail.com>
2023-01-20 20:00:50 +0530

    Task commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System;
+
+namespace ArrayListTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+
+            Console.WriteLine("Enter 3 Person's Name and Age : ");
+            for (int i = 0; i < 3; i++)
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                list.Add(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            foreach (Person p in list)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public override string ToString()
+        {
+            return "Name : " + Name + ", Age : " + Age;
+        }
+    }
+}
